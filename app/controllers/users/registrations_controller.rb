add7fd605636184451ab3b0f7c5b67b4e779956f
Ryotaro.Ieda <ryotaro@RyotaroIedanoMacBook-Air.local>
2022-12-11 17:46:49 +0900

    userの詳細・編集ページ・パスワード変更画面

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,4 +6,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
       redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません。'
     end
   end
+
+  def after_update_path_for(resource)
+    user_path(@user.id)
+  end
+
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      redirect_to user_path(@user.id), alert: 'ゲストユーザーは編集できません。'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_01_081950) do
+ActiveRecord::Schema.define(version: 2022_12_08_110458) do
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_12_01_081950) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "profile_image_id"
+    t.text "introduce"
+    t.string "homesauna"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
