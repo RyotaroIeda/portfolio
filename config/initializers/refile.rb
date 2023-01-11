4a11afe7da0777c61beb015abad3d233ad9e405c
Ryotaro.Ieda <ryotaro@RyotaroIedanoMacBook-Air.local>
2023-01-11 22:59:24 +0900

    s3バケット変更それに伴い環境変数も変更

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,12 +1,12 @@
-require 'refile/s3'
+require "refile/s3"
 
 if Rails.env.production? # 本番環境の場合はS3へアップロード
   aws = {
-    access_key_id: ENV['S3_ACCESS_KEY_ID'],
-    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1', 
-    bucket: 'ryotaroiedaportfolio',
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: "ap-northeast-1",
+    bucket: "saunacampportfolio",
   }
-  Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
-  Refile.store = Refile::S3.new(prefix: 'store', **aws)
+  Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+  Refile.store = Refile::S3.new(prefix: "store", **aws)
 end
