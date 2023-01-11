f70a0bafbd0ffddbfb3096adfbca0bf20401a5cf
Ryotaro.Ieda <ryotaro@RyotaroIedanoMacBook-Air.local>
2023-01-11 23:45:38 +0900

    refileバケット変更

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,7 +5,7 @@ if Rails.env.production? # 本番環境の場合はS3へアップロード
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: "ap-northeast-1",
-    bucket: "ryotaroiedaportfolio",
+    bucket: "iedasauna",
   }
   Refile.cache = Refile::S3.new(prefix: "cache", **aws)
   Refile.store = Refile::S3.new(prefix: "store", **aws)
