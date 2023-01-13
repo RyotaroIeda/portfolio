ed05ba7ba63c3f20d92b4576b90f7797763d132e
Ryotaro.Ieda <ryotaro@RyotaroIedanoMacBook-Air.local>
2023-01-13 15:51:53 +0900

    refile変更

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -7,6 +7,4 @@ if Rails.env.production? # 本番環境の場合
     region: 'ap-northeast-1',
     bucket: 'ryotarosauna',
   }
-  Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
-  Refile.store = Refile::S3.new(prefix: 'store', **aws)
 end
